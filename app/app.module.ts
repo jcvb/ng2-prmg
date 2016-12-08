2c9f51247aaca5f90088ae8b89af7093f96e1f0a
jcvb <jc.valerio.b@gmail.com>
2016-12-08 12:25:45 -0600

    servicio que conecta componentes

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,12 +3,14 @@ import { MenuComponent } from './menu/menu.component';
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports: [ 
-    BrowserModule
+    BrowserModule,
+    FormsModule
    ],
   declarations: [ 
     AppComponent,
--- app/product-list/product-list.component.ts
+++ app/product-list/product-list.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FilterService } from '../filter-service/filter.service';
+import { Subscription }   from 'rxjs/Subscription';
+
 @Component({
     moduleId: module.id,
     selector: 'pm-product-list',
     templateUrl: './product-list.component.html'
 })
-export class ProductListComponent{
+export class ProductListComponent {
+    
+    subscription: Subscription;
+    @Input() filter: string;
+    
+    constructor(private filterService: FilterService) {
+        this.subscription = filterService.filter$.subscribe(
+            filter => {
+                this.filter = filter;
+            }
+        );
+    }
 
 }
